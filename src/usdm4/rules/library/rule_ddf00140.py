c9b9547e3869f37d7369468d59d12125afcc88a7
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2025-03-07 12:01:20 +0100

    Tweak rules

--- src/usdm4/rules/library/rule_ddf00140.py
+++ src/usdm4/rules/library/rule_ddf00140.py
@@ -1,4 +1,7 @@
 from usdm3.rules.library.rule_ddf00140 import RuleDDF00140 as V3Rule
 
 class RuleDDF00140(V3Rule):
-    pass
+    
+    
+    def validate(self, config: dict) -> bool:
+        return self._ct_check(config, "Organization", "type")
