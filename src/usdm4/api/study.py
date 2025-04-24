4e0d15da729e4fb555a1da7f019cdaf8d01e5fd8
daveih <daveih1664dk@gmail.com>
2025-04-24 20:30:24 +0200

    First changes to support editing. Took out the document changes for the moment

--- src/usdm4/api/study.py
+++ src/usdm4/api/study.py
@@ -21,8 +21,26 @@ class Study(ApiBaseModel):
             None,
         )
 
+    def document_templates(self):
+        return [x.templateName for x in self.documentedBy]
+
     def first_version(self) -> StudyVersion:
         try:
             return self.versions[0]
         except Exception:
             return None
+        
+    def summary(self) -> dict:
+        study_version = self.first_version()
+        return {
+            "acronym": study_version.acronym_text(),
+            "full_title": study_version.official_title_text(),
+            "sponsor_protocol_identifier": study_version.sponsor_identifier_text(),
+            "version_number": study_version.versionIdentifier,
+            "version_date": study_version.protocol_date_value(),
+            "trial_phase": ", ".join([x.studyPhase.standardCode.decode for x in study_version.studyDesigns]),
+            "short_title": study_version.short_title_text(),
+            "sponsor_name": study_version.sponsor_name(),
+            "sponsor_address": study_version.sponsor_address(),
+            "sponsor_approval_date": study_version.approval_date_value(),
+        }
--- src/usdm4/api/study_definition_document_version.py
+++ src/usdm4/api/study_definition_document_version.py
@@ -15,8 +15,6 @@ class StudyDefinitionDocumentVersion(ApiBaseModelWithId):
     notes: List[CommentAnnotation] = []
     instanceType: Literal["StudyDefinitionDocumentVersion"]
 
-    DELIMITER = "."
-
     def narrative_content_in_order(self):
         sections = []
         narrative_content = self._first_narrative_content()
@@ -27,55 +25,55 @@ class StudyDefinitionDocumentVersion(ApiBaseModelWithId):
             )
         return sections
 
-    def can_add_sibling_section(self, content: NarrativeContent):
-        potential_section_key = self._increment_section_number(content)
-        return self._section_is_permitted(potential_section_key)
+    # def can_add_sibling_section(self, content: NarrativeContent):
+    #     potential_section_key = self._increment_section_number(content)
+    #     return self._section_is_permitted(potential_section_key)
 
-    def can_add_child_section(self, content: NarrativeContent):
-        potential_section_key = self._child_section_number(content)
-        return self._section_is_permitted(potential_section_key)
+    # def can_add_child_section(self, content: NarrativeContent):
+    #     potential_section_key = self._child_section_number(content)
+    #     return self._section_is_permitted(potential_section_key)
 
-    def add_sibling_section(self, content: NarrativeContent):
-        new_section_key = self._increment_section_number (content)
-        if self._section_is_permitted(new_section_key):
-            self._data[new_section_key] = {
-                "sectionNumber": new_section_key.replace("-", "."),
-                "sectionTitle": "To Be Provided",
-                "name": "",
-                "text": "",
-            }
-            self._write()
-            result = new_section_key
-        else:
-            result = None
+    # def add_sibling_section(self, content: NarrativeContent):
+    #     new_section_key = self._increment_section_number (content)
+    #     if self._section_is_permitted(new_section_key):
+    #         self._data[new_section_key] = {
+    #             "sectionNumber": new_section_key.replace("-", "."),
+    #             "sectionTitle": "To Be Provided",
+    #             "name": "",
+    #             "text": "",
+    #         }
+    #         self._write()
+    #         result = new_section_key
+    #     else:
+    #         result = None
 
-    def add_child_section(self, content: NarrativeContent):
-        new_section_key = self._child_section_number (content)
-        if self._section_is_permitted(new_section_key):
-            self._data[new_section_key] = {
-                "sectionNumber": new_section_key.replace("-", "."),
-                "sectionTitle": "To Be Provided",
-                "name": "",
-                "text": "",
-            }
-            self._write()
-            result = new_section_key
-        else:
-            result = None
+    # def add_child_section(self, content: NarrativeContent):
+    #     new_section_key = self._child_section_number (content)
+    #     if self._section_is_permitted(new_section_key):
+    #         self._data[new_section_key] = {
+    #             "sectionNumber": new_section_key.replace("-", "."),
+    #             "sectionTitle": "To Be Provided",
+    #             "name": "",
+    #             "text": "",
+    #         }
+    #         self._write()
+    #         result = new_section_key
+    #     else:
+    #         result = None
 
-    def _first_narrative_content(self) -> NarrativeContent:
-        return next((x for x in self.contents if not x.previousId and x.nextId), None)
+    # def _first_narrative_content(self) -> NarrativeContent:
+    #     return next((x for x in self.contents if not x.previousId and x.nextId), None)
 
-    def _section_is_permitted(self, content: NarrativeContent):
-        result = True if content: NarrativeContent not in self._data.keys() else False
-        return result
+    # def _section_is_permitted(self, content: NarrativeContent):
+    #     result = True if content: NarrativeContent not in self._data.keys() else False
+    #     return result
     
-    def _increment_section_number(self, content: NarrativeContent):
-        parts = content: NarrativeContent.split(self.DELIMITER)
-        parts[-1] = str(int(parts[-1]) + 1)
-        return self.DELIMITER.join(parts)
+    # def _increment_section_number(self, content: NarrativeContent):
+    #     parts = content: NarrativeContent.split(self.DELIMITER)
+    #     parts[-1] = str(int(parts[-1]) + 1)
+    #     return self.DELIMITER.join(parts)
 
-    def _child_section_number(self, content: NarrativeContent):
-        parts = content: NarrativeContent.split(self.DELIMITER)
-        parts.append('1')
-        return self.DELIMITER.join(parts)
+    # def _child_section_number(self, content: NarrativeContent):
+    #     parts = content: NarrativeContent.split(self.DELIMITER)
+    #     parts.append('1')
+    #     return self.DELIMITER.join(parts)
