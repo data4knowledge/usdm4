d5baf6d6906f49d94dd477ea0080fa84eb383bb6
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2025-07-18 12:21:24 +0200

    Update CT

---
+++ ct_cache.py
@@ -0,0 +1,11 @@
+import os
+from pathlib import Path
+from usdm3.ct.cdisc.library import Library
+from dotenv import load_dotenv
+
+if __name__ == "__main__":
+    root = os.path.join(Path(__file__).parent.resolve(), "src/usdm4")
+    load_dotenv(".development_env")
+    library = Library(root)
+    library._cache.delete()
+    library.load()
